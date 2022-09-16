ac755f6abdb93da2b0389f628b37ec102cf5fff6
Morten Bergseng <bergseng.m@gmail.com>
2022-09-16 12:20:48 +0200

    jwtencdec

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use chrono::{Duration, Utc};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+use std::{thread, time};
+
+// Three registered claims
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String, // Subject
+    iat: usize,  // Issued at
+    exp: usize,  // Expiration date
+    test: String,
+}
+
+fn main() {
+    // CREATE A JSON WEB TOKEN
+    let key = b"secret";
+    let my_iat = Utc::now().timestamp();
+    // exp date 5 seconds into the future
+    let my_exp = Utc::now()
+        .checked_add_signed(Duration::seconds(5))
+        .expect("invalid timestamp")
+        .timestamp();
+
+    // create claims object
+    let my_claims = Claims {
+        sub: "h@d.com".to_owned(),
+        iat: my_iat as usize,
+        exp: my_exp as usize,
+        test: "hello world".to_owned(),
+    };
+
+    // encode the token, passing header, claims object and a key
+    let token = match encode(
+        &Header::default(),
+        &my_claims,
+        &EncodingKey::from_secret(key),
+    ) {
+        Ok(t) => t,
+        Err(_) => panic!(),
+    };
+
+    println!("Token: {}", token);
+
+    // After uncommenting this program will wait for 10 secs so JWT will be invalid and program will panic
+    // println!("Waiting");
+    // thread::sleep(time::Duration::from_secs(10));
+
+    // DECODING A JWT
+    let token_data = match decode::<Claims>(
+        &token,
+        &DecodingKey::from_secret(key),
+        &Validation::default(),
+    ) {
+        Ok(c) => c,
+        Err(err) => {
+            println!("error {:?}", err.kind());
+            panic!();
+        }
+    };
+    println!("\nToken Data: {:?}", token_data);
+}
